4360fb847a1e10da88334b4b67df1c0f0d925c96
Fabio Diniz <fabiomachadodiniz@gmail.com>
2011-06-14 13:22:28 -0300

    Versão inicial da homepage

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 # -*- coding: utf-8 -*-
-from google.appengine.ext import webapp, db
-from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.api import users
-from google.appengine.ext.webapp import template
+#from google.appengine.ext import webapp, db
+#from google.appengine.ext.webapp.util import run_wsgi_app
+#from google.appengine.api import users
+#from google.appengine.ext.webapp import template
+#
+#import os
+#
+#import logging
+#
+#class MainPage(webapp.RequestHandler):
+#    def get(self):
+#        template_values = {
+#            }
+#
+#        path = os.path.join(os.path.dirname(__file__), 'index.html')
+#        self.response.out.write(template.render(path, template_values))
+#
+#application = webapp.WSGIApplication(
+#                                     [('/', MainPage)],
+#                                     debug=True)
+#
+#
+#def main():
+#    run_wsgi_app(application)
+#
+#if __name__ == "__main__":
+#    main()
 
-import os
 
-import logging
+from bottle import route, run, template
 
-class MainPage(webapp.RequestHandler):
-    def get(self):
-        template_values = {
-            }
+@route('/')
+def index():
+    return template('index')
 
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write(template.render(path, template_values))
-
-application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
-                                     debug=True)
-
-def main():
-    run_wsgi_app(application)
-
-if __name__ == "__main__":
-    main()
+# ... build or import your bottle application here ...
+run(server='gae')    
---
+++ views/index.tpl
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Karamachart, parricídio no fluxograma</title>
+<link href='http://fonts.googleapis.com/css?family=Kameron' rel='stylesheet' type='text/css'>
+<link href="/static/css/index.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="leftside">
+<img src="/static/img/karamalogo.png">
+</div>
+<div id="rightside">
+<h1>Karamachart, parricídio no fluxograma</h1>
+</div>
+<div id="footer">
+<a href='http://flavors.me/fabiomdiniz'>Sobre o Autor</a>
+</div>
+
+</body>
+</html>
